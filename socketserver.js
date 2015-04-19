36f914c9c9b8c254bd94a50e3d8fd86a1cce622c
Roger Goldfinger <rgoldfinger@gmail.com>
2015-04-19 13:04:26 -0700

    viewer count

--- socketserver.js
+++ socketserver.js
@@ -2,20 +2,29 @@ var http = require('http');
 var sockjs = require('sockjs');
 var R = require('ramda')
 // http://truongtx.me/2014/06/07/simple-chat-application-using-sockjs/
-var clients = {};
+var _clients = {};
+var _viewerCount = {};
 
 var echo = sockjs.createServer();
 echo.on('connection', function(conn) {
   conn.on('close', function() {
-    delete clients[conn.id];
+    var eventId = conn.eventId;
+    delete _clients[conn.id];
+    _viewerCount[eventId]--;
+    updateViewerCount(eventId);
     console.log('closed');
   });
   conn.on('data', function(message) {
     message = JSON.parse(message);
     if (typeof message === 'object' && message.eventId) {
       var eventId = message.eventId;
-      !clients[eventId] && (clients[eventId] = {});
-      clients[eventId][conn.id] = conn;
+      !_clients[eventId] && (_clients[eventId] = {});
+      conn.eventId = eventId;
+      _clients[eventId][conn.id] = conn;
+
+      if (!_viewerCount[eventId]) _viewerCount[eventId] = 0;
+      _viewerCount[eventId]++;
+      updateViewerCount(eventId);
     }
   })
 });
@@ -25,9 +34,14 @@ echo.installHandlers(server, {prefix: '/ws'});
 
 server.listen(3080, 'localhost');
 
+var updateViewerCount = function(eventId) {
+  console.log(_viewerCount)
+  exports.send('put', 'ViewerCount', eventId, {viewerCount: _viewerCount[eventId]})
+};
+
 exports.send = function(method, type, eventId, data) {
   console.log(method, type, eventId, data);
   R.forEach(function(client) {
     client.write(JSON.stringify({method: method, type: type, data: data}));
-  }, R.values(clients[eventId]))
+  }, R.values(_clients[eventId]))
 };
